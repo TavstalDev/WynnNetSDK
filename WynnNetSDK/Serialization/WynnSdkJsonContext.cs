5c3a9bc01bc3dcb82f86d8e454ff410d47b6b751
tavstal <tavstal@proton.me>
2026-08-31 14:17:22 +0200

    feat: Add JSON serialization support for new item and leaderboard models

--- WynnNetSDK/Serialization/WynnSdkJsonContext.cs
+++ WynnNetSDK/Serialization/WynnSdkJsonContext.cs
@@ -2,6 +2,11 @@ using System.Text.Json.Serialization;
 using Tavstal.WynnNetSDK.Http.Requests.Items.Bodies;
 using Tavstal.WynnNetSDK.Http.Requests.Recipes.Bodies;
 using Tavstal.WynnNetSDK.Models.Common;
+using Tavstal.WynnNetSDK.Models.Items;
+using Tavstal.WynnNetSDK.Models.Items.Icon;
+using Tavstal.WynnNetSDK.Models.Leaderboard;
+using Tavstal.WynnNetSDK.Models.Leaderboard.Metadata;
+using Tavstal.WynnNetSDK.Models.News;
 
 namespace Tavstal.WynnNetSDK.Serialization;
 
@@ -9,4 +14,17 @@ namespace Tavstal.WynnNetSDK.Serialization;
 [JsonSerializable(typeof(ErrorResponse))]
 [JsonSerializable(typeof(ItemSearchRequestBody))]
 [JsonSerializable(typeof(RecipesSearchRequestBody))]
+[JsonSerializable(typeof(Identification))]
+[JsonSerializable(typeof(ItemIcon))]
+[JsonSerializable(typeof(LeaderboardEntry))]
+[JsonSerializable(typeof(LeaderboardGuildEntry))]
+[JsonSerializable(typeof(LeaderboardGuildLegacyEntry))]
+[JsonSerializable(typeof(LeaderboardPlayerEntry))]
+[JsonSerializable(typeof(LeaderboardMetadata))]
+[JsonSerializable(typeof(LeaderboardMetadataCompletionsGambits))]
+[JsonSerializable(typeof(LeaderboardMetadataPlaytime))]
+[JsonSerializable(typeof(LeaderboardMetadataSeason))]
+[JsonSerializable(typeof(LeaderboardMetadataTotalLevelXpPlaytime))]
+[JsonSerializable(typeof(LeaderboardMetadataXpPlaytime))]
+[JsonSerializable(typeof(PublisherPollQuestion))]
 public partial class WynnNetSDKJsonContext : JsonSerializerContext;
