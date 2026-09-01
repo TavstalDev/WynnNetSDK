6010e93e52afde0195ea6b73db539651777316ac
Tavstal <tavstal@gmail.com>
2026-09-01 03:01:14 +0200

    refactor: Simplify WynnEnvironment constructor by removing webUrl parameter

--- WynnNetSDK/Http/WynnEnvironment.cs
+++ WynnNetSDK/Http/WynnEnvironment.cs
@@ -6,19 +6,16 @@ public class WynnEnvironment
 
     public string Token { get; }
     
-    public string WebUrl { get; }
+    public WynnEnvironment(string token) : this("https://api.wynncraft.com", token) {}
     
-    protected WynnEnvironment(string baseUrl, string token, string webUrl)
+    public WynnEnvironment(string baseUrl, string token)
     {
         if (string.IsNullOrEmpty(baseUrl))
             throw new ArgumentException("Base URL cannot be null or empty.", nameof(baseUrl));
         if (string.IsNullOrEmpty(token))
             throw new ArgumentException("Token cannot be null or empty.", nameof(token));
-        if (string.IsNullOrEmpty(webUrl))
-            throw new ArgumentException("Web URL cannot be null or empty.", nameof(webUrl));
         
         BaseUrl = baseUrl;
         Token = token;
-        WebUrl = webUrl;
     }
 }
