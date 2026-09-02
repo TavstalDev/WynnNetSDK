87816197128dd057a63f2679490bf91de8cedc2e
Tavstal <tavstal@gmail.com>
2026-09-02 23:02:18 +0200

    feat: implement search endpoint testing

--- WynnNetSdk.Tests/Tests/Endpoints/SearchTests.cs
+++ WynnNetSdk.Tests/Tests/Endpoints/SearchTests.cs
@@ -1,3 +1,8 @@
+using System.Net;
+using Tavstal.WynnNetSDK.Http.Requests.Search;
+using Tavstal.WynnNetSDK.Models.Guild;
+using Tavstal.WynnNetSDK.Models.Guild.Territory;
+using Tavstal.WynnNetSDK.Models.Items;
 using Tavstal.WynnNetSdk.Tests.Helpers;
 using Xunit.Abstractions;
 
@@ -13,11 +18,50 @@ public class SearchTests : TestBase
     }
 
     [Fact(DisplayName = "Sample 1 - 200 - Search")]
-    public async Task Search_Search()
+    public async Task Search()
     {
         var resource = _resources[0];
         var client = FakeHttpHelpers.CreateClient(resource.Responder);
 
-        // TODO
+        var request = new SearchRequest("Bo");
+        
+        var response = await client.SendAsync(request);
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+        
+        var result = await request.GetResponseBodyAsync(response);
+        result.Should().NotBeNull();
+        
+        
+        _testOutputHelper.WriteLine($"Search -> {result!.Query}");
+        _testOutputHelper.WriteLine("Players: ");
+        foreach (var player in result.Players ?? new Dictionary<string, string>())
+        {
+            _testOutputHelper.WriteLine($"{player.Key} -> {player.Value}");
+        }
+        _testOutputHelper.WriteLine("Guilds: ");
+        foreach (var guild in result.Guilds ?? new Dictionary<string, SearchGuildInfo>())
+        {
+            _testOutputHelper.WriteLine($"{guild.Key} -> {guild.Value.Name}");
+        }
+        _testOutputHelper.WriteLine("Items:");
+        foreach (var item in result.Items ?? new List<Item>())
+        {
+            _testOutputHelper.WriteLine($"{item.DisplayName} -> {item.Type}");
+        }
+        _testOutputHelper.WriteLine("GuildsPrefix:");
+        foreach (var guildsPrefix in result.GuildsPrefix ?? new Dictionary<string, GuildInfo>())
+        {
+            _testOutputHelper.WriteLine($"{guildsPrefix.Key} -> {guildsPrefix.Value.Name}");
+        }
+        _testOutputHelper.WriteLine("Territories:");
+        foreach (var territory in result.Territories ?? new Dictionary<string, TerritoryLocation>())
+        {
+            _testOutputHelper.WriteLine($"{territory.Key} -> {territory.Value}");
+        }
+        _testOutputHelper.WriteLine("Discoveries:");
+        foreach (var discoveries in result.Discoveries ?? new Dictionary<string, TerritoryLocation>())
+        {
+            _testOutputHelper.WriteLine($"{discoveries.Key} -> {discoveries.Value}");
+        }
     }
 }
